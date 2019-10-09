427df10e5fa963991dd28dacf701070b0c7aff65
Anand Baburajan <anandbaburajan@gmail.com>
2019-10-09 18:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, request, json, jsonify
+from flask import Flask, request
 
 recommendations = {
 "Biosphere":['Clean water and sanitation', 'Climate action', 'Life below water', 'Life on land'],
@@ -7,17 +6,18 @@ recommendations = {
 "Economy":['Decent work and economic growth', 'Industry, innovation and infrastructure', 'Reduced inequalities', 'Responsible consumption and production']
 }
 
-
 app = Flask(__name__)
 
-@app.route('/recommend/', methods=['GET', 'POST'])
+# To pass parameters: http://127.0.0.1:5000/recommend?interests=Economy_Society
+
+@app.route('/recommend', methods=['GET', 'POST'])
 def post_():
-    na = request.args.get("anand")
+    na = request.args.get("interests")
     ha=na.split("_")
     rr=[]
     for j in ha:
         rr=rr+recommendations[j]
-    return (" ".join(rr))
+    return "_".join(rr)
 
 if __name__ == '__main__':
     app.run()
