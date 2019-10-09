139e030047e2e50b0993afbb536df77885b00ceb
Anaswara T Rajan <anaswaratrajan@gmail.com>
2019-10-09 13:31:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, json
 
-app = Flask(__name__)
+recommendations = {
+"Biosphere":['Clean water and sanitation', 'Climate action', 'Life below water', 'Life on land'],
+"Society":['No Poverty', 'Zero Hunger', 'Good Health and well-being', 'Quality education', 'Gender Equality', 'Affordable and clean energy', 'Sustainable cities and communities', 'Peace, justice and strong institutions'],
+"Economy":['Decent work and economic growth', 'Industry, innovation and infrastructure', 'Reduced inequalities', 'Responsible consumption and production']
+}
 
-@api.route("/",methods=['POST'])
+api = Flask(__name__)
+
+@api.route("/")
 def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run()
+    api.run()
