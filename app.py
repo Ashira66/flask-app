95bfac82ae2cd890774f736ead87a273663f7338
anaswaratrajan <anaswaratrajan@gmail.com>
2019-10-23 14:49:38 +0530

    input from textfile

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ recommendations = {
 "Society":['No Poverty', 'Zero Hunger', 'Good Health and well-being', 'Quality education', 'Gender Equality', 'Affordable and clean energy', 'Sustainable cities and communities', 'Peace, justice and strong institutions'],
 "Economy":['Decent work and economic growth', 'Industry, innovation and infrastructure', 'Reduced inequalities', 'Responsible consumption and production']
 }
+l=[]
+with open("textfile.txt",'r') as f:
+    l=[]
+    for i in f.readlines():
+        l.append(i.rstrip())
 
 app = Flask(__name__)
 
@@ -14,7 +19,11 @@ def post_():
     na = request.values.getlist('checkbox')
     rr=[]
     for j in na:
-        rr=rr+recommendations[j]
+        for i in l:
+            if j in i:
+                c=i.split(':')
+                rr=rr+c[1]
+        #rr=rr+recommendations[j]
     u = "_".join(rr)
     url = "https://smashsdgs.me/recommend.php?actions=" + u
     return redirect(url, code=302)
