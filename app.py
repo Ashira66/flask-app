e53ada05e4c74be015030ee53e6cef410eea46c8
Ashira66 <56925941+Ashira66@users.noreply.github.com>
2019-10-23 20:19:15 +0530

    Removing unused dictionary from the code

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from flask import Flask, request,redirect
-
-recommendations = {
-"Biosphere":['Clean water and sanitation', 'Climate action', 'Life below water', 'Life on land'],
-"Society":['No Poverty', 'Zero Hunger', 'Good Health and well-being', 'Quality education', 'Gender Equality', 'Affordable and clean energy', 'Sustainable cities and communities', 'Peace, justice and strong institutions'],
-"Economy":['Decent work and economic growth', 'Industry, innovation and infrastructure', 'Reduced inequalities', 'Responsible consumption and production']
-}
 l=[]
 with open("textfile.txt",'r') as f:
     l=[]
